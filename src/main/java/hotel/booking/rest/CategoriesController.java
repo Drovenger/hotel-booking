8341b086b6c7fb66dced103c12d9c6829bbdb2d5
Drovenger <64911983+Drovenger@users.noreply.github.com>
2020-09-14 11:51:58 +0700

    done version 1

---
+++ src/main/java/hotel/booking/rest/CategoriesController.java
@@ -0,0 +1,29 @@
+package hotel.booking.rest;
+
+import hotel.booking.HotelServiceDAO;
+import hotel.booking.model.ApiResponse;
+import hotel.booking.model.Category;
+import hotel.booking.util.HotelAppErrorCodes;
+import hotel.booking.util.HotelAppErrorMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CategoriesController {
+
+	@Autowired
+	HotelServiceDAO hotelServiceDAO;
+
+	@RequestMapping(value = "/categories", method = RequestMethod.GET)
+	public ResponseEntity<Object> getCategories() {
+		Category category = hotelServiceDAO.getCategories();
+		if (category == null || category.getCategories().isEmpty()) {
+			return new ResponseEntity<>(new ApiResponse(HotelAppErrorCodes.NOT_FOUND, HotelAppErrorMessage.NOT_FOUND), HttpStatus.OK);
+		}
+		return new ResponseEntity<>(category, HttpStatus.OK);
+	}
+}
