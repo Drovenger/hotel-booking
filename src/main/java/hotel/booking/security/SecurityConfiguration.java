f9f3171a2a4d9699af7afa7db224a43f360ec9a5
Hieu <piutar>
2020-09-15 16:56:49 +0700

    them icon phan title

--- src/main/java/hotel/booking/security/SecurityConfiguration.java
+++ src/main/java/hotel/booking/security/SecurityConfiguration.java
@@ -47,6 +47,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     public void configure(WebSecurity web) throws Exception {
         web.ignoring().antMatchers("/index")
+                .antMatchers("/static/**")
                 .antMatchers("/welcome")
                 .antMatchers("/")
                 .antMatchers("/search")
